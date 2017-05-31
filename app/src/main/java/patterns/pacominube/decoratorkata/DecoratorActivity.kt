0ad89a64fbb5670c824c3f6e4bf634e51b6a2b85
pacoalface <franciscoalfacemartin@gmail.com>
2017-05-31 17:51:17 +0200

    Base project files added

---
+++ app/src/main/java/patterns/pacominube/decoratorkata/DecoratorActivity.kt
@@ -0,0 +1,17 @@
+package patterns.pacominube.decoratorkata
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DecoratorActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_decorator)
+
+    findViewById(R.id.simple_alert).setOnClickListener({  })
+    findViewById(R.id.power_alert).setOnClickListener({  })
+
+
+  }
+}
