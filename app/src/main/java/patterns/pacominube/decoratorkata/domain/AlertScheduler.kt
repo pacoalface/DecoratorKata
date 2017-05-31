6096cb89c06c124f5b76147d164e4edf223e323e
pacoalface <franciscoalfacemartin@gmail.com>
2017-05-31 18:22:04 +0200

    SilentAlert scheduler implemented

--- app/src/main/java/patterns/pacominube/decoratorkata/domain/AlertScheduler.kt
+++ app/src/main/java/patterns/pacominube/decoratorkata/domain/AlertScheduler.kt
@@ -1,9 +1,12 @@
 package patterns.pacominube.decoratorkata.domain
 
+import android.content.Context
+import android.view.View
+
 /**
  * Created by pacominube on 31/5/17.
  */
 interface AlertScheduler {
-  fun scheduleNotification()
-  fun scheduleToast()
+  fun scheduleNotification(view: View)
+  fun scheduleToast(context : Context)
 }
---
+++ app/src/main/java/patterns/pacominube/decoratorkata/domain/SilentAlertScheduler.kt
@@ -0,0 +1,21 @@
+package patterns.pacominube.decoratorkata.domain
+
+import android.content.Context
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.Toast
+
+/**
+ * Created by pacominube on 31/5/17.
+ */
+class SilentAlertScheduler : AlertScheduler {
+
+  override fun scheduleToast(context: Context) {
+    Toast.makeText(context, "Tu valoracion sobre este rincon se ha hecho de forma correcta", Toast.LENGTH_SHORT).show()
+  }
+
+  override fun scheduleNotification(view: View) {
+    Snackbar.make(view,"Tu valoracion sobre este rincon se ha hecho de forma correcta", Snackbar.LENGTH_SHORT)
+  }
+
+}
