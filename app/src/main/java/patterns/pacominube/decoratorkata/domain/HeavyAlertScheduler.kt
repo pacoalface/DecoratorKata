b39ceb359b1ef61691014e69414e53dfbbb3225f
pacoalface <franciscoalfacemartin@gmail.com>
2017-06-06 08:49:09 +0200

    Improved alerter method

--- app/src/main/java/patterns/pacominube/decoratorkata/domain/HeavyAlertScheduler.kt
+++ app/src/main/java/patterns/pacominube/decoratorkata/domain/HeavyAlertScheduler.kt
@@ -3,7 +3,9 @@ package patterns.pacominube.decoratorkata.domain
 import android.app.Activity
 import android.content.Context
 import android.media.MediaPlayer
+import android.view.View
 import com.tapadoo.alerter.Alerter
+import patterns.pacominube.decoratorkata.NotificationActivity
 import patterns.pacominube.decoratorkata.R
 
 /**
@@ -21,6 +23,8 @@ class HeavyAlertScheduler(val mediumAlertScheduler: MediumAlertScheduler) : Aler
   fun scheduleWindow(context: Context) {
     Alerter.create(context as Activity)
         .setTitle("Genial!")
+        .enableVibration(true)
+        .setOnClickListener({ NotificationActivity.startActivity(context) })
         .setText("Tu valoracion sobre este rincon se ha hecho de forma correcta")
         .show()
   }
